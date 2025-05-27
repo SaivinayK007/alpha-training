d68df96b01ff4c881c95f86cc48840f19ae4e9d0
KarriEmmanuelWatson <watsoemmanuel04@gmail.com>
2025-05-27 09:21:16 +0530

    updated

--- day 7/BinaryTreeZigZag.py
+++ day 7/BinaryTreeZigZag.py
@@ -5,7 +5,7 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def zigzagLevelOrder(self, root):
         if not root :
             return []
         queue = [root] 
